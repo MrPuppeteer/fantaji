61c23702af274a9f0d43169701e1f4113fec5379
MrPuppeteer <nuriksabagas@gmail.com>
2021-08-24 13:06:47 +0700

    add session & flash

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
@@ -31,6 +33,25 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+
+const sessionConfig = {
+    secret: 'temp',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use('/characters', characters);
 
--- routes/characters.js
+++ routes/characters.js
@@ -26,28 +26,39 @@ router.get('/new', (req, res) => {
 router.post('/', validateCharacter, catchAsync(async (req, res) => {
     const character = new Character(req.body.character);
     await character.save();
+    req.flash('success', 'Successfully made a new character!');
     res.redirect(`/characters/${character._id}`);
 }));
 
 router.get('/:id', catchAsync(async (req, res) => {
     const character = await Character.findById(req.params.id);
+    if (!campground) {
+        req.flash('error', 'Cannot find the character!');
+        return res.redirect('/campgrounds');
+    }
     res.render('characters/show', { character });
 }));
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const character = await Character.findById(req.params.id);
+    if (!campground) {
+        req.flash('error', 'Cannot find the character!');
+        return res.redirect('/campgrounds');
+    }
     res.render('characters/edit', { character });
 }));
 
 router.put('/:id', validateCharacter, catchAsync(async (req, res) => {
     const { id } = req.params;
     const character = await Character.findByIdAndUpdate(id, { ...req.body.character });
+    req.flash('success', 'Successfully updated the character!');
     res.redirect(`/characters/${character._id}`);
 }));
 
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Character.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted the character!');
     res.redirect('/characters');
 }));
 
